683cbaa1f77b60c6de7a65b20134239eba1cdb3a
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2023-01-22 11:48:17 -0300

    feat: command to open Docker on linux case user does'nt have it

--- detectos.sh
+++ detectos.sh
@@ -36,7 +36,7 @@ case "$OSTYPE" in
             sudo apt install curl  &&\
             sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&\
             sudo chmod +x /usr/local/bin/docker-compose &&\
-            wait open -a Docker.app &&\
+            wait systemctl --user start docker-desktop &&\
             docker ps;
           elif [[ ! $(which docker-compose) && ! $(docker-compose -v) ]]
           then
